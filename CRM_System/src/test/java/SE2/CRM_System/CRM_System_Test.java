2fa41cea06229e793e63f84c572c172cdf2536f4
jh170 <jh170@hmd-stuttgart.de>
2016-06-26 20:31:01 +0200

    Tests halbwegs...

--- CRM_System/src/test/java/SE2/CRM_System/CRM_System_Test.java
+++ CRM_System/src/test/java/SE2/CRM_System/CRM_System_Test.java
@@ -1,11 +1,24 @@
 package SE2.CRM_System;
 
+import mainFunctionality.FactoryKunde;
 import mainFunctionality.Kundenliste;
 
 import org.junit.Test;
+
+import gui.MainGUI;
+import javafx.collections.ObservableList;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.web.HTMLEditor;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+
 import org.junit.Assert;
 
 import persons.Admin;
+import persons.Kunde;
 
 public class CRM_System_Test {
 
@@ -41,16 +54,46 @@ public class CRM_System_Test {
 		
 		Assert.assertFalse(expectedResult.equals(actualResult));
 	}
-	//FEHLER!!!
+	
+	@Test
+	public void testKundentableIsEmpty() {
+		Kundenliste.listeDerKunden.addAll(FactoryKunde.listeDerKunden);
+		 Assert.assertFalse(Kundenliste.listeDerKunden.isEmpty());	
+	}
+	
+	@Test
+	public void testMainGUILöschen() {
+		Assert.assertEquals(MainGUI.kundentable.getItems() == MainGUI.kundentable.getItems().remove(1), null);
+	}
+	
+	@Test 
+	public void testMainGUIAddKunde() {
+		
+		final ObservableList<Kunde> expectedResult = MainGUI.kundentable.getItems();
+
+		HTMLEditor Notiz2 = null;
+		FactoryKunde.add("Kunde", "Yamashi", "Andano", "Tendostraße", 65, 564657465, "Tokio", "Japan", "5745354357", 19, Notiz2);
+		
+		final ObservableList<Kunde> actualResult = MainGUI.kundentable.getItems();
+		
+		Assert.assertFalse(expectedResult == actualResult);
+	}
+	
 	//@Test
-	public void testDriverWrongKundennummer() {
-		final int expectedResult = 2;
-		final int actualResult = Kundenliste.listeDerKunden.get(2).Kundennummer;
+	public void testKundenlisteSearch() {
 		
-		Assert.assertTrue(expectedResult == actualResult);
+		TextField suchfeld;
+	//	suchfeld.setText("Peter");
 		
+	//	Assert.assertFalse(Kundenliste.search(suchfeld, MainGUI.kundentable).equals(MainGUI.kundentable.getItems()));
+	}
+	
+	@Test(expected = NumberFormatException.class)
+	public void testException() {
+		 new ArrayList<Object>().get(0);
 	}
 	
 	
 	
+
 }
